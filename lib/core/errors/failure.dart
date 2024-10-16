3e3fb3bc043495cac5e836baabe436fbebcb2927
piernitas <piernitas@gmail.com>
2024-10-16 14:59:11 -0400

    beep boop

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -3,16 +3,6 @@ abstract class Failure {
   const Failure({required this.errorMessage});
 }
 
-// class ServerFailure extends Failure {
-//   ServerFailure({required String errorMessage})
-//       : super(errorMessage: errorMessage);
-// }
-
-// class CacheFailure extends Failure {
-//   CacheFailure({required String errorMessage})
-//       : super(errorMessage: errorMessage);
-// }
-
 class ServerFailure extends Failure {
   ServerFailure({required super.errorMessage});
 }
